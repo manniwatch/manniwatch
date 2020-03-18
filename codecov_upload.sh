4a0e2a8caab71c0c4054e888020ace6e55ecedef
DonMahallem <donmahallem@users.noreply.github.com>
2020-03-18 19:25:13 +0100

    m

--- codecov_upload.sh
+++ codecov_upload.sh
@@ -12,7 +12,7 @@ do
     echo "Uploading $package_name - $flag_name"
     echo "Coverage File: $coverage_filename"
     export COVERALLS_FLAG_NAME=$flag_name
-    cat $coverage_filename | coveralls -v
+    cat $coverage_filename | coveralls -v ./packages/$package_name/src
     echo "Uploaded"
 done
 unset COVERALLS_PARALLEL
