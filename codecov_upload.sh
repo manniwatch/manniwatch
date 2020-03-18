44cd952c1f1964a800ff80cf55c7e1ee74852692
DonMahallem <donmahallem@users.noreply.github.com>
2020-03-18 17:06:25 +0100

    Setup

--- codecov_upload.sh
+++ codecov_upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 COVERAGE_FILES=(./packages/*/coverage/lcov.info)
 COVERAGE_FILES+=(./packages/*/coverage/**/lcov.info)
+npm install coveralls -g
 echo "Found ${#COVERAGE_FILES[@]} Coverage Files"
 for coverage_filename in "${COVERAGE_FILES[@]}"
 do
@@ -8,6 +9,7 @@ do
     flag_name=$(echo "$package_name" | sed -r 's/(^|\W)(\w)/\U\2/g')
     echo "Uploading $package_name - $flag_name"
     echo "Coverage File: $coverage_filename"
-    bash <(curl -s https://codecov.io/bash) -F $flag_name -f $coverage_filename -n Unittest -y ./.codecov.yml
+    export COVERALLS_FLAG_NAME=$flag_name
+    cat $coverage_filename | coveralls
     echo "Uploaded"
 done
