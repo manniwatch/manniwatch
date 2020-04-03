d35c5554b1c07737ef7ee2cd341504eed5cfe20b
DonMahallem <donmahallem@users.noreply.github.com>
2020-04-03 13:42:18 +0200

    feat(vehicle-cache): implement safeQueryData

--- packages/vehicle-cache/src/vehicle-cache.spec.ts
+++ packages/vehicle-cache/src/vehicle-cache.spec.ts
@@ -4,31 +4,69 @@
 
 import { expect } from 'chai';
 import 'mocha';
-import { TestScheduler } from 'rxjs/testing';
 import * as sinon from 'sinon';
 import { VehicleCache } from './vehicle-cache';
+import { ManniWatchApiClient } from '@manniwatch/api-client';
+import { Observable } from 'rxjs';
+import { IVehicleLocationList } from '@manniwatch/api-types';
 
 describe('vehicle-cache', (): void => {
     describe('VehicleCache', (): void => {
-        let testScheduler: TestScheduler;
         let testInstance: VehicleCache;
         let sandbox: sinon.SinonSandbox;
+        let testApiClient: sinon.SinonStubbedInstance<ManniWatchApiClient>;
         before((): void => {
             sandbox = sinon.createSandbox();
+            testApiClient = sandbox.createStubInstance(ManniWatchApiClient);
         });
         beforeEach((): void => {
-            testInstance = new VehicleCache(undefined as any);
-            testScheduler = new TestScheduler((actual: any, expected: any): void => {
-                // asserting the two objects are equal
-                // e.g. using chai.
-                expect(actual).deep.equal(expected);
-            });
+            testInstance = new VehicleCache(testApiClient as any);
         });
         afterEach((): void => {
             sandbox.resetHistory();
         });
         describe('safeQueryData()', (): void => {
-            it('needs impl');
+            /**
+             * Testing doesnt work with marbles
+             * https://github.com/ReactiveX/rxjs/pull/745
+             */
+            let nextSpy: sinon.SinonSpy;
+            before((): void => {
+                nextSpy = sandbox.spy();
+            });
+            [0, 12059].forEach((testTimestamp: number): void => {
+                it('should resolve with result from promise with timestamp: ' + testTimestamp, (done: Mocha.Done): void => {
+                    const testPromise: any = (): Promise<IVehicleLocationList> => {
+                        return Promise.resolve('KKKK' as any);
+                    };
+                    testApiClient.getVehicleLocations.callsFake(testPromise);
+                    const foreverStream$: Observable<IVehicleLocationList> = testInstance.safeQueryData(testTimestamp);
+                    // Omitting this arg may crash the test suite.
+                    foreverStream$
+                        .subscribe(nextSpy, done, (): void => {
+                            expect(nextSpy.callCount).to.equal(1);
+                            expect(nextSpy.getCall(0).args).to.deep.equal(['KKKK']);
+                            expect(testApiClient.getVehicleLocations.callCount).to.equal(1);
+                            expect(testApiClient.getVehicleLocations.getCall(0).args).to.deep.equal(['RAW', testTimestamp]);
+                            done();
+                        });
+                });
+                it('should resolve EMPTY if the promise rejects with timestamp: ' + testTimestamp, (done: Mocha.Done): void => {
+                    const testPromise: any = (): Promise<IVehicleLocationList> => {
+                        return Promise.reject('KKKK' as any);
+                    };
+                    testApiClient.getVehicleLocations.callsFake(testPromise);
+                    const foreverStream$: Observable<IVehicleLocationList> = testInstance.safeQueryData(testTimestamp);
+                    // Omitting this arg may crash the test suite.
+                    foreverStream$
+                        .subscribe(nextSpy, done, (): void => {
+                            expect(nextSpy.callCount).to.equal(0);
+                            expect(testApiClient.getVehicleLocations.callCount).to.equal(1);
+                            expect(testApiClient.getVehicleLocations.getCall(0).args).to.deep.equal(['RAW', testTimestamp]);
+                            done();
+                        });
+                });
+            });
         });
     });
 });
--- packages/vehicle-cache/src/vehicle-cache.ts
+++ packages/vehicle-cache/src/vehicle-cache.ts
@@ -4,38 +4,23 @@
 
 import { ManniWatchApiClient, PositionType } from '@manniwatch/api-client';
 import { IVehicleLocationList } from '@manniwatch/api-types';
-import { defer, from, of, BehaviorSubject, Observable } from 'rxjs';
-import { concatMap, delay, retryWhen, share, switchMap, tap } from 'rxjs/operators';
+import { defer, from, EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 export class VehicleCache {
     public constructor(public client: ManniWatchApiClient,
         public readonly queryDelay: number = 15000,
         public readonly positionType: PositionType = 'RAW') {
 
     }
-    private lastUpdateSubject: BehaviorSubject<number> = new BehaviorSubject(0);
     public polling: boolean = true;
     public safeQueryData(lastUpdate: number): Observable<IVehicleLocationList> {
         const networkRequest: Observable<IVehicleLocationList> = defer((): Observable<IVehicleLocationList> => {
             return from(this.client.getVehicleLocations(this.positionType, lastUpdate));
         });
         return networkRequest
-            .pipe(retryWhen((errors: Observable<any>): Observable<any> => {
-                return errors.pipe(delay(this.queryDelay * 2));
+            .pipe(catchError((): Observable<any> => {
+                return EMPTY;
             }));
-    }
-
-    public createPollingObservable(): Observable<IVehicleLocationList> {
-        return this.lastUpdateSubject.pipe(
-            concatMap((lastUpdate: number, index: number): Observable<number> => {
-                if (index < 1) {
-                    return of(lastUpdate);
-                } else {
-                    return of(lastUpdate).pipe(delay(this.queryDelay));
-                }
-            }),
-            switchMap((lastUpdate: number): Observable<IVehicleLocationList> => this.safeQueryData(lastUpdate)),
-            tap((response: IVehicleLocationList): void => this.lastUpdateSubject.next(response.lastUpdate)),
-            share());
     }
 
     public start(): void {
