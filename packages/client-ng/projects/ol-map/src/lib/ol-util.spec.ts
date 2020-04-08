13f003a5e88c77acb4a3af2967013faff83a3d75
DonMahallem <donmahallem@users.noreply.github.com>
2020-04-08 17:12:28 +0200

    fix

---
+++ packages/client-ng/projects/ol-map/src/lib/ol-util.spec.ts
@@ -0,0 +1,10 @@
+/*!
+ * Source https://github.com/manniwatch/manniwatch Package: client-ng
+ */
+
+import { OlUtil } from './ol-util';
+describe('projects/ol-map/src/lib/ol-util', (): void => {
+    it('Test', (): void => {
+        expect(OlUtil.convertArcMSToCoordinate).not.toBeUndefined();
+    });
+});
