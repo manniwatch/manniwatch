076a40020cd8a3de5b7513312c315805e629c5fb
DonMahallem <donmahallem@users.noreply.github.com>
2020-04-28 21:12:00 +0200

    feat(api-proxy-router): add trip/:id/passages route
    add missing route

--- packages/api-proxy-router/src/endpoints/trip.ts
+++ packages/api-proxy-router/src/endpoints/trip.ts
@@ -2,10 +2,23 @@
  * Source https://github.com/manniwatch/manniwatch Package: api-proxy-router
  */
 
-import { ManniWatchApiClient } from '@manniwatch/api-client';
+import { ManniWatchApiClient, StopMode } from '@manniwatch/api-client';
 import * as prom from '@manniwatch/express-utils';
 import * as express from 'express';
+import { Schema } from 'jsonschema';
 
+export const passagesSchema: Schema = {
+    additionalProperties: false,
+    properties: {
+        mode: {
+            description: 'departure mode',
+            enum: ['departure', 'arrival'],
+            id: 'mode',
+            type: 'string',
+        },
+    },
+    type: 'object',
+};
 export const createTripRouter: (apiClient: ManniWatchApiClient) => express.Router =
     (apiClient: ManniWatchApiClient): express.Router => {
         const router: express.Router = express.Router();
@@ -20,5 +33,20 @@ export const createTripRouter: (apiClient: ManniWatchApiClient) => express.Route
         router.get('/:id([a-z0-9A-Z\-\+]+)/route', (req: express.Request, res: express.Response, next: express.NextFunction): void => {
             prom.promiseToResponse(apiClient.getRouteByTripId(req.params.id), res, next);
         });
+
+        /**
+         * @api {get} /trip/:id/passages Request Trip Passages
+         * @apiName GetTripPassages
+         * @apiGroup Trip
+         *
+         * @apiParam {String} id Vehicle id
+         * @apiVersion 0.5.0
+         */
+        router.get('/:id([a-z0-9A-Z\-\+]+)/passages',
+            prom.validateRequest({ query: passagesSchema }),
+            (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+                const departureMode: StopMode = req.query.mode as StopMode || 'departure';
+                prom.promiseToResponse(apiClient.getTripPassages(req.params.id, departureMode), res, next);
+            });
         return router;
     };
