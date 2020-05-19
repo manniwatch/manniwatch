735d083881384b6240bb1769836147adb2212495
donmahallem <donmahallem@users.noreply.github.com>
2020-05-19 14:04:18 +0200

    fix(client-ng): await app isStable before polling passages (#401)
    * fix(client-ng): await app isStable before polling passages
    
    * test(client-ng): update tests to await isStable
    
    * style(client-ng): fix lint stylings

--- packages/client-ng/src/app/routes/trip-passages/trip-passages.service.ts
+++ packages/client-ng/src/app/routes/trip-passages/trip-passages.service.ts
@@ -2,11 +2,11 @@
  * Source https://github.com/manniwatch/manniwatch Package: client-ng
  */
 
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { ITripPassages } from '@manniwatch/api-types';
 import { merge, timer, BehaviorSubject, Observable, Subject } from 'rxjs';
-import { flatMap, map, scan, switchMap, take, tap } from 'rxjs/operators';
+import { first, flatMap, map, scan, switchMap, take, tap } from 'rxjs/operators';
 import { ApiService, TimestampedVehicleLocation, VehicleService } from 'src/app/services';
 import { IPassageStatus, TripPassagesUtil, UpdateStatus } from './trip-util';
 
@@ -16,23 +16,28 @@ export class TripPassagesService {
     private readonly statusSubject: BehaviorSubject<IPassageStatus>;
     constructor(private route: ActivatedRoute,
         private apiService: ApiService,
-        private vehicleService: VehicleService) {
+        private vehicleService: VehicleService,
+        private appRef: ApplicationRef) {
         this.statusSubject = new BehaviorSubject(route.snapshot.data.tripPassages);
         this.statusObservable = this.createStatusObservable(this.statusSubject);
     }
 
     public createStatusObservable(statusSubject: Subject<IPassageStatus>): Observable<IPassageStatus> {
         const refreshObservable: Observable<IPassageStatus> = this.createRefreshPollObservable(statusSubject);
-        return merge(this.route.data.pipe(map((data: Data): ITripPassages => data.tripPassages)), refreshObservable)
-            .pipe(scan((acc: IPassageStatus, val: IPassageStatus, idx: number): IPassageStatus => {
-                if (val.failures > 0) {
-                    const newVal: IPassageStatus = Object.assign({}, val);
-                    newVal.failures = val.failures + acc.failures;
-                    return newVal;
-                }
-                return val;
-            }),
-                tap((newStatus: IPassageStatus): void => statusSubject.next(newStatus)));
+        return this.appRef.isStable
+            .pipe(first(),
+                flatMap((): Observable<IPassageStatus> => {
+                    return merge(this.route.data.pipe(map((data: Data): ITripPassages => data.tripPassages)), refreshObservable)
+                        .pipe(scan((acc: IPassageStatus, val: IPassageStatus, idx: number): IPassageStatus => {
+                            if (val.failures > 0) {
+                                const newVal: IPassageStatus = Object.assign({}, val);
+                                newVal.failures = val.failures + acc.failures;
+                                return newVal;
+                            }
+                            return val;
+                        }),
+                            tap((newStatus: IPassageStatus): void => statusSubject.next(newStatus)));
+                }));
     }
 
     public createRefreshPollObservable(statusSubject: Subject<IPassageStatus>): Observable<IPassageStatus> {
