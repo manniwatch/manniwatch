baff48071974ce2bcb4c7d0a58cff395706ca662
donmahallem <donmahallem@users.noreply.github.com>
2020-05-20 14:23:56 +0200

    feat(api-types): add PositionType and StopMode type defintions (#413)
    * feat(api-types): add PositionType and StopMode type defintions
    
    re #412
    
    * refactor(api-types): replace lose type definitions of VehicleCategory
    
    previously string literals were used in some places
    
    fix #411

--- packages/api-types/src/type-util.ts
+++ packages/api-types/src/type-util.ts
@@ -3,3 +3,11 @@
  */
 
 export type VehicleCategory = string | 'bus' | 'tram';
+/**
+ * Stop Mode to query
+ */
+export type StopMode = 'arrival' | 'departure';
+/**
+ * Should coordinates be corrected or unaltered
+ */
+export type PositionType = 'CORRECTED' | 'RAW';
