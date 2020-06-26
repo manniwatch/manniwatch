b8f7456ead3c8576287d55ef544dea4f4f40c2a4
donmahallem <donmahallem@users.noreply.github.com>
2020-06-26 14:40:16 +0200

    fix(deps): bump angular to version 10

--- packages/client-ng/src/app/modules/openlayers/index.ts
+++ packages/client-ng/src/app/modules/openlayers/index.ts
@@ -2,6 +2,6 @@
  * Source https://github.com/manniwatch/manniwatch Package: client-ng
  */
 
-export { OlMapComponent } from './ol-map.directive';
+export { AbstractOlMapDirective } from './abstract-ol-map.directive';
 export { OlStaticMapDirective, IStaticMapData } from './ol-static-map.directive';
 export { OlStaticMapModule } from './ol-static-map.module';
