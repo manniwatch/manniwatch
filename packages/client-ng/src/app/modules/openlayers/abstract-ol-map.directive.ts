7e3957b468bdea79c784c6d9417ea7378fb0ae13
DonMahallem <donmahallem@users.noreply.github.com>
2020-07-17 15:50:20 +0200

    feat(client-ng): implement dark map

--- packages/client-ng/src/app/modules/openlayers/abstract-ol-map.directive.ts
+++ packages/client-ng/src/app/modules/openlayers/abstract-ol-map.directive.ts
@@ -9,8 +9,11 @@ import { defaults, Interaction } from 'ol/interaction';
 import TileLayer from 'ol/layer/Tile';
 import { fromLonLat } from 'ol/proj';
 import XYZ from 'ol/source/XYZ';
+import MVT from 'ol/format/MVT';
 import { Subscription } from 'rxjs';
 import { SettingsService } from 'src/app/services';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
 
 @Directive()
 export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy, OnChanges {
@@ -26,14 +29,25 @@ export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy
             // Seems to be necessary to run ngZone updates EVERY SINGLE TIME!!!! the map is firing a drag event
             this.map = new Map({
                 interactions: defaults(),
-                layers: [
+                layers: [/*
                     new TileLayer({
                         source: new XYZ({
                             attributions: '<a target="_blank" rel="noopener noreferrer" href="https://www.openstreetmap.org/copyright">©OpenStreetMap</a>-Contributors',
                             attributionsCollapsible: false,
                             url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                         }),
-                    }),
+                    }),*/
+                    new VectorTileLayer({
+                        declutter: false,
+                        source: new VectorTileSource({
+                            attributions: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
+                                '© <a href="https://www.openstreetmap.org/copyright">' +
+                                'OpenStreetMap contributors</a>',
+                            maxZoom: 14,
+                            format: new MVT(),
+                            url: "https://d1u6l41epxe4hw.cloudfront.net/tiles/{z}/{x}/{y}.pbf",
+                        }),
+                    })
                 ],
                 target: this.elRef.nativeElement,
                 view: new View({
