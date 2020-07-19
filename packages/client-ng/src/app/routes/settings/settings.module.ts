84e02ea92c0579f8c1aa2543cb1c926abd3a15ca
DonMahallem <donmahallem@users.noreply.github.com>
2020-07-19 18:30:35 +0200

    style(client-ng): fix lint issue

--- packages/client-ng/src/app/routes/settings/settings.module.ts
+++ packages/client-ng/src/app/routes/settings/settings.module.ts
@@ -8,8 +8,8 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RetryDialogModule } from 'src/app/modules/common/retry-dialog';
-import { SettingsComponent } from './settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { SettingsComponent } from './settings.component';
 import { SettingsResolver } from './settings.resolver';
 
 /**
