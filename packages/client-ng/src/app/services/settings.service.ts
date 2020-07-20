1ea7b1b2b162f9e00bf1a7d7283da49d265e6099
DonMahallem <donmahallem@users.noreply.github.com>
2020-07-20 10:37:16 +0200

    feat(client-ng): add theme options to settings service

--- packages/client-ng/src/app/services/settings.service.ts
+++ packages/client-ng/src/app/services/settings.service.ts
@@ -3,9 +3,11 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Subscriber } from 'rxjs';
+import { Subscriber, BehaviorSubject, Observable, combineLatest } from 'rxjs';
 import { environment } from 'src/environments';
+import { createCssThemeWatcher } from './css-theme-watcher';
 import { Theme } from './theme';
+import { map } from 'rxjs/operators';
 
 // tslint:disable:max-classes-per-file
 export class SettingsLoadSubscriber extends Subscriber<void> {
@@ -29,12 +31,56 @@ export class SettingsLoadSubscriber extends Subscriber<void> {
 )
 export class SettingsService {
 
+    public readonly themeObservable: Observable<Theme>;
+    private themeSubject: BehaviorSubject<Theme>;
+    private cssThemeObservable: Observable<Theme>;
     constructor() {
-
+        this.themeSubject = new BehaviorSubject(this.getThemePreference());
+        this.cssThemeObservable = createCssThemeWatcher();
+        this.themeObservable = combineLatest([this.themeSubject, this.cssThemeObservable])
+            .pipe(map((themes: [Theme, Theme]): Theme => {
+                if (themes[0] === Theme.DEFAULT) {
+                    return themes[1];
+                }
+                return themes[0];
+            }));
     }
 
     public setTheme(theme: Theme): void {
+        this.themeSubject.next(theme);
+        this.setThemePreference(theme);
+    }
 
+    /**
+     * Persists the theme preference in localStorage
+     * @param theme theme to store
+     */
+    protected setThemePreference(theme: Theme): void {
+        switch (theme) {
+            case Theme.DARK:
+                localStorage.setItem('theme', 'dark');
+                break;
+            case Theme.LIGHT:
+                localStorage.setItem('theme', 'light');
+                break;
+            default:
+                localStorage.removeItem('theme');
+                break;
+        }
+    }
+
+    /**
+     * Retrieves the theme preference from localStorage
+     */
+    protected getThemePreference(): Theme {
+        switch (localStorage.getItem('theme')) {
+            case 'dark':
+                return Theme.DARK;
+            case 'light':
+                return Theme.LIGHT;
+            default:
+                return Theme.DEFAULT;
+        }
     }
 
     public getInitialMapCenter(): [number, number] {
