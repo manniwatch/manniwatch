311cea1ccc1e7bbc718d5183cc54cbfe6d660e86
DonMahallem <donmahallem@users.noreply.github.com>
2020-07-24 15:09:51 +0200

    test(client-ng): add ThemeSelectorComponent method tests

--- packages/client-ng/src/app/routes/settings/theme-selector/theme-selector.component.spec.ts
+++ packages/client-ng/src/app/routes/settings/theme-selector/theme-selector.component.spec.ts
@@ -5,7 +5,9 @@
 import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { SettingsService } from 'src/app/services';
+import { NEVER } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+import { SettingsService, Theme } from 'src/app/services';
 import { ThemeSelectorComponent } from './theme-selector.component';
 
 // tslint:disable:max-classes-per-file
@@ -38,8 +40,10 @@ describe('src/routes/settings/theme-selector/theme-selector.component.ts', (): v
         let fixture: ComponentFixture<ThemeSelectorComponent>;
         let app: ThemeSelectorComponent;
         let setThemeSpy: jasmine.Spy<jasmine.Func>;
+        let themeObservableSubscribeSpy: jasmine.Spy<jasmine.Func>;
         beforeAll((): void => {
-            setThemeSpy = jasmine.createSpy();
+            setThemeSpy = jasmine.createSpy('setTheme');
+            themeObservableSubscribeSpy = jasmine.createSpy('themeObservable.subscribe');
         });
         beforeEach(async((): void => {
             TestBed.configureTestingModule({
@@ -57,6 +61,9 @@ describe('src/routes/settings/theme-selector/theme-selector.component.ts', (): v
                         provide: SettingsService,
                         useValue: {
                             setTheme: setThemeSpy,
+                            get themeObservable(): any {
+                                return themeObservableSubscribeSpy();
+                            },
                         },
                     },
                 ],
@@ -66,6 +73,7 @@ describe('src/routes/settings/theme-selector/theme-selector.component.ts', (): v
         }));
         afterEach((): void => {
             setThemeSpy.calls.reset();
+            themeObservableSubscribeSpy.calls.reset();
         });
 
         it('should create the app', async((): void => {
@@ -73,6 +81,50 @@ describe('src/routes/settings/theme-selector/theme-selector.component.ts', (): v
         }));
         describe('layout', (): void => {
             it('needs to be implemented');
+        });
+        describe('methods', (): void => {
+            describe('selectTheme', (): void => {
+                it('should call through to setTheme on settingservice', (): void => {
+                    expect(setThemeSpy).not.toHaveBeenCalled();
+                    app.selectTheme(Theme.DARK);
+                    expect(setThemeSpy).toHaveBeenCalledTimes(1);
+                    expect(setThemeSpy).toHaveBeenCalledWith(Theme.DARK);
+                });
+            });
+            describe('onSelectionChange', (): void => {
+                it('should call through to setTheme on settingservice', (): void => {
+                    expect(setThemeSpy).not.toHaveBeenCalled();
+                    app.onSelectionChange({ option: { value: Theme.DARK } } as any);
+                    expect(setThemeSpy).toHaveBeenCalledTimes(1);
+                    expect(setThemeSpy).toHaveBeenCalledWith(Theme.DARK);
+                });
+            });
+            describe('ngOnInit', (): void => {
+                it('should subscribe correctly', (): void => {
+                    expect((app as any).themeSubscription).not.toBeDefined();
+                    app.theme = Theme.LIGHT;
+                    themeObservableSubscribeSpy.and.returnValue(NEVER.pipe(startWith(Theme.DARK)));
+                    expect(themeObservableSubscribeSpy).toHaveBeenCalledTimes(0);
+                    app.ngOnInit();
+                    expect(themeObservableSubscribeSpy).toHaveBeenCalledTimes(1);
+                    expect(app.theme).toEqual(Theme.DARK as any, 'Expected theme to be set to dark');
+                });
+            });
+            describe('ngOnDestroy', (): void => {
+                it('should not throw without subscription', (): void => {
+                    expect((): void => {
+                        app.ngOnDestroy();
+                    }).not.toThrow();
+                });
+                it('should unsubscribe correctly', (): void => {
+                    const unsubscribeSpy: jasmine.Spy<jasmine.Func> = jasmine.createSpy('unsubscribe');
+                    (app as any).themeSubscription = { unsubscribe: unsubscribeSpy };
+                    expect((): void => {
+                        app.ngOnDestroy();
+                    }).not.toThrow();
+                    expect(unsubscribeSpy).toHaveBeenCalledTimes(1);
+                });
+            });
         });
     });
 });
