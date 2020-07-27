2ada3a1aa0bcc06a2e771a79ccebcd162a3b4f62
DonMahallem <donmahallem@users.noreply.github.com>
2020-07-28 01:04:30 +0200

    chore(assets): update gulp file

--- packages/assets/gulpfile.js
+++ packages/assets/gulpfile.js
@@ -1,14 +1,15 @@
-const gulp = require('gulp')
-const flatMap = require('flat-map').default
+const gulp = require('gulp');
+const flatMap = require('flat-map').default;
 const del = require('del');
-const path = require('path')
-const scaleImages = require('gulp-scale-images')
+const path = require('path');
+const scaleImages = require('gulp-scale-images');
+const rename = require("gulp-rename");
 
 const build_manifest_icons = () => {
-    const twoVariantsPerFile = (file, cb) => {
+    const generateOutputFormats = (file, cb) => {
         const sizes = [72, 96, 128, 144, 152, 192, 384, 512];
         const outputFiles = sizes.map((size) => {
-            const pngFile = file.clone()
+            const pngFile = file.clone();
             pngFile.scale = {
                 maxWidth: size,
                 maxHeight: size,
@@ -21,14 +22,17 @@ const build_manifest_icons = () => {
         cb(null, outputFiles)
     }
     return gulp.src('src/app_icon.svg')
-        .pipe(flatMap(twoVariantsPerFile))
+        .pipe(rename({
+            basename: "launcher",
+        }))
+        .pipe(flatMap(generateOutputFormats))
         .pipe(scaleImages((output, scale, cb) => {
             const fileName = path.basename(output.path, output.extname) + '_' + // strip extension
                 scale.maxWidth + 'x' + scale.maxHeight + '.' +
                 scale.format || output.extname;
             cb(null, fileName)
         }))
-        .pipe(gulp.dest('dist/manifest'));
+        .pipe(gulp.dest('dist/launcher'));
 };
 
 const clean = () => {
