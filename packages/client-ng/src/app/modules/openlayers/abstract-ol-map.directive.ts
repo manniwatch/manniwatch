5137147a3be0cd5c3253474b6f7aac17d1c48b9c
DonMahallem <donmahallem@users.noreply.github.com>
2020-08-13 10:35:09 +0200

    feat(client-ng): enable theme switching for map

--- packages/client-ng/src/app/modules/openlayers/abstract-ol-map.directive.ts
+++ packages/client-ng/src/app/modules/openlayers/abstract-ol-map.directive.ts
@@ -4,22 +4,26 @@
 
 import { AfterViewInit, Directive, ElementRef, NgZone, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { Collection, Map, View } from 'ol';
+import stylefunction from 'ol-mapbox-style/dist/stylefunction';
 import { Coordinate } from 'ol/coordinate';
-import { defaults, Interaction } from 'ol/interaction';
-import TileLayer from 'ol/layer/Tile';
-import { fromLonLat } from 'ol/proj';
-import XYZ from 'ol/source/XYZ';
 import MVT from 'ol/format/MVT';
-import { Subscription } from 'rxjs';
-import { SettingsService } from 'src/app/services';
+import { defaults, Interaction } from 'ol/interaction';
+import BaseTileLayer from 'ol/layer/BaseTile';
 import VectorTileLayer from 'ol/layer/VectorTile';
+import { fromLonLat } from 'ol/proj';
 import VectorTileSource from 'ol/source/VectorTile';
+import { Subscription } from 'rxjs';
+import { SettingsService, Theme } from 'src/app/services';
+import { runOutsideZone } from 'src/app/util/rxjs';
+import { DARK_THEME, LIGHT_THEME } from './theme';
 
 @Directive()
 export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy, OnChanges {
 
     private map: Map;
     private locationSubscription: Subscription;
+    private mBackgroundMapLayer: BaseTileLayer;
+    private themeSubscription: Subscription;
     constructor(private elRef: ElementRef,
         public readonly zone: NgZone,
         public readonly settings: SettingsService) {
@@ -27,27 +31,11 @@ export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy
     public ngAfterViewInit(): void {
         this.zone.runOutsideAngular((): void => {
             // Seems to be necessary to run ngZone updates EVERY SINGLE TIME!!!! the map is firing a drag event
+            this.mBackgroundMapLayer = this.createMapLayer();
             this.map = new Map({
                 interactions: defaults(),
-                layers: [/*
-                    new TileLayer({
-                        source: new XYZ({
-                            attributions: '<a target="_blank" rel="noopener noreferrer" href="https://www.openstreetmap.org/copyright">©OpenStreetMap</a>-Contributors',
-                            attributionsCollapsible: false,
-                            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        }),
-                    }),*/
-                    new VectorTileLayer({
-                        declutter: false,
-                        source: new VectorTileSource({
-                            attributions: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
-                                '© <a href="https://www.openstreetmap.org/copyright">' +
-                                'OpenStreetMap contributors</a>',
-                            maxZoom: 14,
-                            format: new MVT(),
-                            url: "https://d1u6l41epxe4hw.cloudfront.net/tiles/{z}/{x}/{y}.pbf",
-                        }),
-                    })
+                layers: [
+                    this.mBackgroundMapLayer,
                 ],
                 target: this.elRef.nativeElement,
                 view: new View({
@@ -59,11 +47,27 @@ export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy
                 }),
             });
             this.map.updateSize();
+            this.applyTheme(this.settings.theme);
             this.onAfterSetView(this.map);
         });
     }
 
+    public createMapLayer(): BaseTileLayer {
+        return new VectorTileLayer({
+            declutter: false,
+            source: new VectorTileSource({
+                attributions: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> ' +
+                    '© <a href="https://www.openstreetmap.org/copyright">' +
+                    'OpenStreetMap contributors</a>',
+                format: new MVT(),
+                maxZoom: 14,
+                url: 'https://d1u6l41epxe4hw.cloudfront.net/tiles/{z}/{x}/{y}.pbf',
+            }),
+        });
+    }
+
     public panMapTo(panTo: Coordinate, zoom?: number): void {
+        NgZone.assertNotInAngularZone();
         if (this.getMap()) {
             this.getMap().updateSize();
             this.getMap().getView().animate({
@@ -73,6 +77,24 @@ export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy
             });
         }
     }
+
+    /**
+     * Selected theme
+     * @param theme theme to set
+     */
+    public applyTheme(theme: Theme): void {
+        NgZone.assertNotInAngularZone();
+        if (theme === Theme.DARK) {
+            stylefunction(this.mBackgroundMapLayer, DARK_THEME, 'openmaptiles');
+        } else {
+            stylefunction(this.mBackgroundMapLayer, LIGHT_THEME, 'openmaptiles');
+        }
+    }
+
+    public get backgroundMapLayer(): BaseTileLayer {
+        return this.mBackgroundMapLayer;
+    }
+
     public ngOnChanges(changes: SimpleChanges): void {
         if (this.map) {
             this.map.updateSize();
@@ -88,7 +110,14 @@ export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy
     }
 
     public onAfterSetView(map: Map): void {
-
+        this.themeSubscription = this.settings
+            .themeObservable
+            .pipe(runOutsideZone(this.zone))
+            .subscribe({
+                next: (val: Theme): void => {
+                    this.applyTheme(val);
+                },
+            });
     }
 
     public getMap(): Map | undefined {
@@ -99,6 +128,9 @@ export abstract class AbstractOlMapDirective implements AfterViewInit, OnDestroy
         this.map.dispose();
         if (this.locationSubscription) {
             this.locationSubscription.unsubscribe();
+        }
+        if (this.themeSubscription) {
+            this.themeSubscription.unsubscribe();
         }
     }
 }
