1c6485fa8c39022c2d031ea733ee47188cc702bd
DonMahallem <donmahallem@users.noreply.github.com>
2020-08-13 11:17:01 +0200

    feat(client-ng): add optional MapProvider option to environment

--- packages/client-ng/src/environments/environment.base.ts
+++ packages/client-ng/src/environments/environment.base.ts
@@ -2,6 +2,8 @@
  * Source https://github.com/manniwatch/manniwatch Package: client-ng
  */
 
+import BaseTileLayer from 'ol/layer/BaseTile';
+
 export type BackendType = 'nginx' | 'trapeze';
 export interface IEnvironmentBase {
     readonly apiEndpoint: string;
@@ -12,6 +14,11 @@ export interface IEnvironmentBase {
             lon: number;
         };
         zoom: number;
+        /**
+         * Map Provider to be used
+         * Defaults to 'osm'
+         */
+        mapProvider?: 'osm' | BaseTileLayer;
     };
     readonly production: boolean;
     readonly pwa?: boolean;
