8da74393023f21dee0d1d57c12d5e2c2c84029e9
DonMahallem <donmahallem@users.noreply.github.com>
2020-08-18 22:14:08 +0200

    feat(assets): export mask and no mask launcher icons

--- packages/assets/gulpfile.js
+++ packages/assets/gulpfile.js
@@ -5,7 +5,7 @@ const path = require('path');
 const gulpSharp = require('@donmahallem/gulp-sharp').gulpSharp;
 const rename = require("gulp-rename");
 
-const build_launcher_icons = () => {
+const build_launcher_icons_mask = () => {
     const generateOutputFormats = (file, cb) => {
         const sizes = [72, 96, 128, 144, 152, 192, 384, 512];
         const outputFiles = sizes.map((size) => {
@@ -34,7 +34,39 @@ const build_launcher_icons = () => {
                 format: 'png',
             }
         }))
-        .pipe(gulp.dest('dist/launcher'));
+        .pipe(gulp.dest('dist/launcher/mask'));
+};
+
+const build_launcher_icons_no_mask = () => {
+    const generateOutputFormats = (file, cb) => {
+        const sizes = [72, 96, 128, 144, 152, 192, 384, 512];
+        const outputFiles = sizes.map((size) => {
+            const pngFile = file.clone();
+            pngFile.sharp_config = {
+                transform: {
+                    resize: {
+                        height: size,
+                        width: size,
+                    }
+                }
+            };
+            return pngFile;
+        });
+
+        cb(null, outputFiles)
+    }
+    return gulp.src('src/app_icon_no_clip.svg')
+        .pipe(rename({
+            basename: "launcher",
+        }))
+        .pipe(flatMap(generateOutputFormats))
+        .pipe(gulpSharp({
+            config: { density: 1200 },
+            transform: {
+                format: 'png',
+            }
+        }))
+        .pipe(gulp.dest('dist/launcher/no_mask'));
 };
 
 const clean = () => {
@@ -45,7 +77,7 @@ const copy_svgs = () => {
     return gulp.src('src/*.svg')
         .pipe(gulp.dest('dist/svg'));
 };
-
+const build_launcher_icons = gulp.parallel(build_launcher_icons_mask, build_launcher_icons_no_mask);
 const build = gulp.parallel(build_launcher_icons, copy_svgs);
 exports.build_launcher_icons = build_launcher_icons;
 exports.copy_svgs = copy_svgs;
