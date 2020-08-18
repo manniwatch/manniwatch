313c38442921353dd0aaf344693b00d86174ddf9
donmahallem <donmahallem@users.noreply.github.com>
2020-08-18 09:44:25 +0200

    feat(assets): add @manniwatch/assets package (#742)
    * feat(assets): add @manniwatch/assets package
    
    * chore(assets): add gulp
    
    * chore(assets): remove package descriptors
    
    * chore(assets): add gulp dependency
    
    * chore(assets): add flat-map dependency
    
    * chore(assets): add gulp-scale-images dependency
    
    * chore: add gulpfile.js
    
    * chore(assets): add @types/gulp dependency
    
    * chore(assets): add del
    
    * build: expand gulpfile
    
    * chore: add sharp dependency
    
    * chore(assets): add gulp-rename dependency
    
    * chore(assets): update gulp file
    
    * chore(assets): fix readme_config
    
    * chore(assets): update readme
    
    * chore(deps-dev): add @donmahallem/gulp-sharp to @manniwatch/assets
    
    * chore(deps-dev): bump @donmahallem/gulp-sharp to 0.2.1
    
    * chore(deps-dev): bump @donmahallem/gulp-sharp
    
    * feat(assets): use @donmahallem/gulp-sharp to scale
    
    * build(assets): extend base readme_config.json
    
    * build(assets): add npm version script

---
+++ packages/assets/gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const flatMap = require('flat-map').default;
+const del = require('del');
+const path = require('path');
+const gulpSharp = require('@donmahallem/gulp-sharp').gulpSharp;
+const rename = require("gulp-rename");
+
+const build_launcher_icons = () => {
+    const generateOutputFormats = (file, cb) => {
+        const sizes = [72, 96, 128, 144, 152, 192, 384, 512];
+        const outputFiles = sizes.map((size) => {
+            const pngFile = file.clone();
+            pngFile.sharp_config = {
+                transform: {
+                    resize: {
+                        height: size,
+                        width: size,
+                    }
+                }
+            };
+            return pngFile;
+        });
+
+        cb(null, outputFiles)
+    }
+    return gulp.src('src/app_icon.svg')
+        .pipe(rename({
+            basename: "launcher",
+        }))
+        .pipe(flatMap(generateOutputFormats))
+        .pipe(gulpSharp({
+            config: { density: 1200 },
+            transform: {
+                format: 'png',
+            }
+        }))
+        .pipe(gulp.dest('dist/launcher'));
+};
+
+const clean = () => {
+    return del(['dist/**', '!dist']);
+};
+
+const build = gulp.parallel(build_launcher_icons);
+exports.build_launcher_icons = build_launcher_icons;
+exports.build = build;
+exports.clean = clean;
+exports.default = gulp.series(clean, build);
