0b7708b04d6b37b3a24ae57ffe9a18809e676679
DonMahallem <donmahallem@users.noreply.github.com>
2021-01-18 09:25:53 +0100

    feat(express-utils): add axios specific AxiosError handling

--- packages/express-utils/src/promise-to-response.ts
+++ packages/express-utils/src/promise-to-response.ts
@@ -2,26 +2,42 @@
  * Source https://github.com/manniwatch/manniwatch Package: express-utils
  */
 
+ import {AxiosError}from'axios';
 import { NextFunction, Response } from 'express';
-import { FullResponse, RequestPromise } from 'request-promise-native';
 
-type MethodType = <T>(prom: Promise<T> | RequestPromise<T>,
+type MethodType = <T>(prom: Promise<T> ,
     res: Response,
     next?: NextFunction) => void;
 /**
  * takes promises and passes them on to an express response
  */
-export const promiseToResponse: MethodType = <T>(prom: Promise<T> | RequestPromise<T>,
+export const promiseToResponse: MethodType = <T>(prom: Promise<T>,
     res: Response,
     next?: NextFunction): void => {
     prom
         .then((value: T): void => {
             res.status(200).json(value);
         })
-        .catch((err: any | FullResponse): void => {
+        .catch((err: any | AxiosError): void => {
             if (next) {
                 next(err);
-            } else {
+            } else if(err.isAxiosError===true){
+                const axiosError:AxiosError=err;
+                if(axiosError.response){
+                    const code: number = axiosError.response.status || 500;
+                    res.status(code).json({
+                        error: true,
+                        statusCode: code,
+                    });
+                }else{
+                    const code: number =  500;
+                    res.status(code).json({
+                        error: true,
+                        message:axiosError.message,
+                        statusCode: code,
+                    });
+                }
+            }else{
                 const code: number = err.statusCode || 500;
                 res.status(code).json({
                     error: true,
