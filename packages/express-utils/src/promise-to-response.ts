08f10ca252b2bd30141a42951e1e52ef303ee232
DonMahallem <donmahallem@users.noreply.github.com>
2021-01-18 12:24:24 +0100

    fix(express-utils): handle AxiosError case

--- packages/express-utils/src/promise-to-response.ts
+++ packages/express-utils/src/promise-to-response.ts
@@ -2,10 +2,10 @@
  * Source https://github.com/manniwatch/manniwatch Package: express-utils
  */
 
- import {AxiosError}from'axios';
+import { AxiosError } from 'axios';
 import { NextFunction, Response } from 'express';
 
-type MethodType = <T>(prom: Promise<T> ,
+type MethodType = <T>(prom: Promise<T>,
     res: Response,
     next?: NextFunction) => void;
 /**
@@ -21,23 +21,14 @@ export const promiseToResponse: MethodType = <T>(prom: Promise<T>,
         .catch((err: any | AxiosError): void => {
             if (next) {
                 next(err);
-            } else if(err.isAxiosError===true){
-                const axiosError:AxiosError=err;
-                if(axiosError.response){
-                    const code: number = axiosError.response.status || 500;
-                    res.status(code).json({
-                        error: true,
-                        statusCode: code,
-                    });
-                }else{
-                    const code: number =  500;
-                    res.status(code).json({
-                        error: true,
-                        message:axiosError.message,
-                        statusCode: code,
-                    });
-                }
-            }else{
+            } else if (err.isAxiosError === true) {
+                const axiosError: AxiosError = err;
+                const code: number = axiosError?.response?.status || 500;
+                res.status(code).json({
+                    error: true,
+                    statusCode: code,
+                });
+            } else {
                 const code: number = err.statusCode || 500;
                 res.status(code).json({
                     error: true,
