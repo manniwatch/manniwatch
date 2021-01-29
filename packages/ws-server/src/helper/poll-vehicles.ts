c90f5b442f4cad308fe5627aed666846d6e2aab5
DonMahallem <donmahallem@users.noreply.github.com>
2021-01-29 11:31:09 +0100

    feat(ws-server): add pollVehicles

---
+++ packages/ws-server/src/helper/poll-vehicles.ts
@@ -0,0 +1,25 @@
+import { from, NEVER, Observable, of, OperatorFunction } from "rxjs";
+import { catchError, map, mergeMap } from "rxjs/operators";
+import { ManniWatchApiClient } from "@manniwatch/api-client";
+import { IPollError, IPollResponse, PollResult, PollResultStatus } from "./types";
+import { IVehicleLocationList, PositionType } from "@manniwatch/api-types";
+
+export const pollVehicles = (client: ManniWatchApiClient, positionType: PositionType = 'RAW'): OperatorFunction<number, PollResult> => {
+    return (source: Observable<number>): Observable<PollResult> => {
+        return source.pipe(mergeMap((lastUpdate: number): Observable<PollResult> => {
+            return from(client.getVehicleLocations(positionType, lastUpdate))
+                .pipe(map((response: IVehicleLocationList): IPollResponse => {
+                    return {
+                        status: PollResultStatus.SUCCESS,
+                        result: response,
+                    }
+                }),
+                    catchError((err: any): Observable<IPollError> => {
+                        return of({
+                            status: PollResultStatus.ERROR,
+                            error: err,
+                        });
+                    }))
+        }));
+    };
+}
