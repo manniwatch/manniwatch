ae057072ec47c234a3f10f0449bc38123709c11f
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-02 12:13:48 +0100

    style(vehicle-cache): fix styling in types

--- packages/vehicle-cache/src/types/index.ts
+++ packages/vehicle-cache/src/types/index.ts
@@ -1,1 +1,5 @@
+/*!
+ * Source https://github.com/manniwatch/manniwatch Package: vehicle-cache
+ */
+
 export { IQuerySettings } from './query-settings';
--- packages/vehicle-cache/src/types/query-settings.ts
+++ packages/vehicle-cache/src/types/query-settings.ts
@@ -1,6 +1,10 @@
-import { PositionType } from "@manniwatch/api-types";
+/*!
+ * Source https://github.com/manniwatch/manniwatch Package: vehicle-cache
+ */
+
+import { PositionType } from '@manniwatch/api-types';
 
 export interface IQuerySettings {
-    type?: PositionType,
-    lastUpdate?: number,
+    type?: PositionType;
+    lastUpdate?: number;
 }
