5b95451aef7b1f4dac69e6f414fed5635d987169
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-03 11:33:20 +0100

    docs(vehicle-cache): add docs to queryVehicles

--- packages/vehicle-cache/src/observable/query-vehicles.ts
+++ packages/vehicle-cache/src/observable/query-vehicles.ts
@@ -6,8 +6,16 @@ import { ManniWatchApiClient } from '@manniwatch/api-client';
 import { IVehicleLocationList, PositionType } from '@manniwatch/api-types';
 import { from, Observable } from 'rxjs';
 
-export const queryVehicles = (client: ManniWatchApiClient,
+/**
+ * Wraps the ManniWatchApiClient getVehicleLocations method into an Observable
+ * @param client ManniWatchApiClient instance
+ * @param type PositionType. Default 'RAW'
+ * @param lastUpdate lastUpdate. Default 0
+ */
+export const queryVehicles: (client: ManniWatchApiClient,
     type?: PositionType,
-    lastUpdate?: number): Observable<IVehicleLocationList> => {
-    return from(client.getVehicleLocations(type || 'RAW', lastUpdate || 0));
-};
+    lastUpdate?: number) => Observable<IVehicleLocationList> = (client: ManniWatchApiClient,
+        type: PositionType = 'RAW',
+        lastUpdate: number = 0): Observable<IVehicleLocationList> => {
+        return from(client.getVehicleLocations(type, lastUpdate));
+    };
