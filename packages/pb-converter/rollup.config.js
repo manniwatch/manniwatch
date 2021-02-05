12cad6f91d777d618758d1fb3fe66d93f5e85f69
donmahallem <donmahallem@users.noreply.github.com>
2021-02-05 15:47:55 +0100

    build(pb-converter): use global rollup config (#1478)
    * build(pb-converter): use global default rollup file
    
    * build(pb-converter): update build npm scripts
    
    * chore(deps): remove duplicate devDependencies
    
    these dependencies are already available at root level
    
    * chore(deps): fix package-lock
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/pb-converter/rollup.config.js
+++ packages/pb-converter/rollup.config.js
@@ -1,36 +1,4 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-
+import defaultRollup from './../../rollup.default';
 import pkg from './package.json';
-//import { terser } from "rollup-plugin-terser";
-export default {
-    input: 'src/index.ts', // our source file
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            sourcemap: true,
-            exports: "named"
-        },
-        {
-            file: pkg.module,
-            format: 'es', // the preferred format
-            sourcemap: true,
-            exports: "named"
-        }
-    ],
-    external: [
-        ...Object.keys(pkg.dependencies || {})
-    ],
-    plugins: [
-        nodeResolve(),
-        commonjs({
-            include: [
-                'node_modules/**'
-            ],
-        }),
-        typescript({
-            tsconfig: './tsconfig.json',
-        })]
-};
+
+export default defaultRollup(pkg);
