4e7bb48d9050eba40c991734c1fd34749c0e9764
ò_ó <donmahallem@users.noreply.github.com>
2021-03-10 14:49:42 +0100

    feat(client-types): add IElectronInterface (#1621)
    * feat(client-types): add IElectronInterface
    
    * style(client-types): fix ending semicolons

--- packages/client-types/src/index.ts
+++ packages/client-types/src/index.ts
@@ -6,8 +6,8 @@ import { IBoundingBox, ManniWatchApiClient } from '@manniwatch/api-client';
 import {
     ITripPassages,
 } from '@manniwatch/api-types';
+import { IEnvironmentBase } from './environment.base';
 export { IEnvironmentBase } from './environment.base';
-
 export type TripInfoWithId = ITripPassages & { tripId: string };
 export type IBounds = IBoundingBox;
 type ApiMethods = Exclude<keyof ManniWatchApiClient, 'endpoint' |
@@ -19,3 +19,8 @@ type ApiMethods = Exclude<keyof ManniWatchApiClient, 'endpoint' |
 export type ApiService = { [k in ApiMethods]: ManniWatchApiClient[k] } & {
     getTripPassages(...args: Parameters<ManniWatchApiClient['getTripPassages']>): Promise<TripInfoWithId>,
 };
+
+export interface IElectronInterface {
+    api: ApiService;
+    environment: IEnvironmentBase;
+}
