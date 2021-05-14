c271e4492e3c90979b9f0e94ec7f5a59456457e0
DonMahallem <donmahallem@users.noreply.github.com>
2021-05-14 02:30:18 +0200

    fix(client-ng): bump @angular/material to v12

--- packages/client-ng/src/environments/app-version.ts
+++ packages/client-ng/src/environments/app-version.ts
@@ -6,5 +6,5 @@
  * Reads the angular app version from the package file
  * @returns The current app version
  */
-import { version } from '../../package.json';
-export const APP_VERSION: string = version;
+import * as packageInfo from '../../package.json';
+export const APP_VERSION: string = packageInfo.version;
