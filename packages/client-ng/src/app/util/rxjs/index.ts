d7b3c80b4ffc52cc9875c49a3471931a08ba5b4f
DonMahallem <donmahallem@users.noreply.github.com>
2021-09-03 11:56:49 +0200

    chore(client-ng): remove redundant code

--- packages/client-ng/src/app/util/rxjs/index.ts
+++
@@ -1,6 +0,0 @@
-/*!
- * Source https://github.com/manniwatch/manniwatch Package: client-ng
- */
-
-export { runInsideZone } from './run-inside-zone';
-export { runOutsideZone } from './run-outside-zone';
