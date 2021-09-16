9b9089867518be8ffb3f69e774ba9b50511f0d8a
DonMahallem <donmahallem@users.noreply.github.com>
2021-09-16 17:53:19 +0200

    fix(client-ng): use config.example.json in publish

--- packages/client-ng/scripts/prepare.js
+++ packages/client-ng/scripts/prepare.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 fs.copyFileSync('./src/environments/environment.example.ts', './src/environments/environment.ts');
 fs.copyFileSync('./src/environments/environment.example.ts', './src/environments/environment.prod.ts');
 fs.copyFileSync('./src/environments/environment.example.ts', './src/environments/environment.dev.ts');
+fs.copyFileSync('./config/config.example.ts', './config/config.ts');
