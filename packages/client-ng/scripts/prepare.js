7ee318e25a16c66876b693b9ad14b665b12b4c49
DonMahallem <donmahallem@users.noreply.github.com>
2021-09-16 18:06:34 +0200

    fix(client-ng): fix typo in copy cmd

--- packages/client-ng/scripts/prepare.js
+++ packages/client-ng/scripts/prepare.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 fs.copyFileSync('./src/environments/environment.example.ts', './src/environments/environment.ts');
 fs.copyFileSync('./src/environments/environment.example.ts', './src/environments/environment.prod.ts');
 fs.copyFileSync('./src/environments/environment.example.ts', './src/environments/environment.dev.ts');
-fs.copyFileSync('./config/config.example.ts', './config/config.ts');
+fs.copyFileSync('./config/config.example.json', './config/config.json');
