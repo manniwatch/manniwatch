0637c8616c06a226424b98db86b81563540832c3
DonMahallem <donmahallem@users.noreply.github.com>
2021-10-31 10:38:02 +0100

    style(api-proxy-router): update jsdoc config to allow apiDoc tags
    currently for every apidoc tag a warning is thrown

--- packages/api-proxy-router/.eslintrc.js
+++ packages/api-proxy-router/.eslintrc.js
@@ -2,8 +2,50 @@ module.exports = {
     "extends": [
         "@donmahallem/eslint-config"
     ],
-    "rules": {},
+    "rules": {
+        "jsdoc/check-tag-names": [
+            "error" | "warn",
+            {
+                "definedTags": [
+                    "api",
+                    "apiParam",
+                    "apiQuery"
+                ]
+            }
+        ]
+    },
     "parserOptions": {
         "project": "./tsconfig.spec.json",
     },
+    "settings": {
+        "jsdoc": {
+            "structuredTags": {
+                "apiName": {
+                    "required": ["name"]
+                },
+                "apiGroup": {
+                    "required": ["name"]
+                },
+                "apiParam": {
+                    "required": ["name"]
+                },
+                "apiVersion": {
+                    "required": ["name"]
+                },
+                // "apiQuery": { "required": false },
+                "api": {
+                    "type": [
+                        "get",
+                        "post",
+                        "put",
+                        "delete",
+                        "head"
+                    ],
+                    "required": [
+                        "type"
+                    ]
+                }
+            }
+        }
+    }
 };
