6cf3bb444c0cfade2b864d30588e21779ebf5a7d
ò_ó <donmahallem@users.noreply.github.com>
2022-01-01 11:28:36 +0100

    test(client-ng): add OlUtil tests (#2654)
    * test(client-ng): add createStyleByType test
    
    * test(client-ng): add more test cases for OlUtil.convertArcMSToCoordinate
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/client-ng/src/app/util/ol/ol-util.spec.ts
+++ packages/client-ng/src/app/util/ol/ol-util.spec.ts
@@ -3,7 +3,8 @@
  * Source https://manniwatch.github.io/manniwatch/
  */
 
-import { OlUtil } from '.';
+import { fromLonLat } from 'ol/proj';
+import { OlUtil, TrapezeCoord } from '.';
 
 describe('util/ol/ol-util', (): void => {
     describe('OlUtil', (): void => {
@@ -15,6 +16,27 @@ describe('util/ol/ol-util', (): void => {
                         lon: 0,
                     })
                 ).toBeTruthy();
+            });
+            it('should throw an error if invalid coordinate format is provided', (): void => {
+                expect((): void => {
+                    OlUtil.convertArcMSToCoordinate({
+                        lat: 0,
+                        longitude: 0,
+                    } as any);
+                }).toThrowError('Invalid coordinates');
+            });
+            it('should convert coordinates with latitude and longitude properties', (): void => {
+                expect(
+                    OlUtil.convertArcMSToCoordinate({
+                        latitude: 3600000,
+                        longitude: 3600000,
+                    })
+                ).toEqual(fromLonLat([1, 1]));
+            });
+        });
+        describe('createStyleByType(feature)', (): void => {
+            it('should return same result for stopPoint and point', (): void => {
+                expect(OlUtil.createStyleByType('stopPoint')).toEqual(OlUtil.createStyleByType('stop'));
             });
         });
     });
