55062ce86e1124f1fe3a8da0d55114730712f095
donmahallem <donmahallem@users.noreply.github.com>
2024-01-10 15:00:10 +0100

    feat(client-ng): reduce components overhead for passages

--- packages/client-ng/src/app/routes/trip-passages/trip-passages.module.ts
+++ packages/client-ng/src/app/routes/trip-passages/trip-passages.module.ts
@@ -7,16 +7,16 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderBoxModule } from 'src/app/modules/header-box';
 import { OlStaticMapModule } from 'src/app/modules/openlayers';
-import { TripPassagesListItemComponent } from './trip-passages-list-item.component';
 import { TripPassagesListComponent } from './trip-passages-list.component';
 import { TripPassagesRoutingModule } from './trip-passages-routing.module';
 import { TripPassagesComponent } from './trip-passages.component';
 import { TripPassagesResolver } from './trip-passages.resolver';
 @NgModule({
-    declarations: [TripPassagesComponent, TripPassagesListComponent, TripPassagesListItemComponent],
-    imports: [CommonModule, MatIconModule, MatListModule, TripPassagesRoutingModule, HeaderBoxModule, OlStaticMapModule],
+    declarations: [TripPassagesComponent, TripPassagesListComponent],
+    imports: [CommonModule, MatIconModule, MatListModule, TripPassagesRoutingModule, HeaderBoxModule, OlStaticMapModule,MatSlideToggleModule],
     providers: [TripPassagesResolver],
 })
 export class TripPassagesModule {}
