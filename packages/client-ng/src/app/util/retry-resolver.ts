65efc72b4141a2e0a0d00d48084296f0c02a40cc
donmahallem <donmahallem@users.noreply.github.com>
2025-03-07 12:43:28 +0100

    fix(deps): bump axios and rxjs deps

--- packages/client-ng/src/app/util/retry-resolver.ts
+++ packages/client-ng/src/app/util/retry-resolver.ts
@@ -4,8 +4,8 @@
  */
 
 import { HttpErrorResponse } from '@angular/common/http';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
-import { EMPTY, from, mergeMap, Observable, of, retryWhen, throwError } from 'rxjs';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot, MaybeAsync } from '@angular/router';
+import { EMPTY, from, mergeMap, Observable, of, retry, throwError } from 'rxjs';
 import { AppDialogService } from '../services';
 
 export type ErrorTypes = Error | HttpErrorResponse;
@@ -14,35 +14,37 @@ export abstract class RetryResolver<T> implements Resolve<T> {
         public router: Router,
         public dialog: AppDialogService
     ) {}
-    public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<T> {
+    public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<T> {
         return this.createLoader(route, state).pipe(
-            retryWhen((obs: Observable<ErrorTypes>): Observable<boolean> => {
-                return obs.pipe(
-                    mergeMap((err: ErrorTypes) => {
-                        if (err instanceof HttpErrorResponse) {
-                            if (err.status === 404) {
-                                return this.navigate(['error', 'not-found']).pipe(
-                                    mergeMap((navigated: boolean): Observable<never> => {
-                                        return EMPTY;
-                                    })
-                                );
-                            } else if (err.status >= 500 && err.status < 600) {
-                                return this.dialog
-                                    .getRetryDialog({
-                                        msg: 'Error',
-                                    })
-                                    .pipe(
-                                        mergeMap((it: boolean): Observable<true> => {
-                                            return it ? of(it) : throwError((): ErrorTypes => err);
+            retry({
+                delay: (obs: Observable<ErrorTypes>): Observable<boolean> => {
+                    return obs.pipe(
+                        mergeMap((err: ErrorTypes) => {
+                            if (err instanceof HttpErrorResponse) {
+                                if (err.status === 404) {
+                                    return this.navigate(['error', 'not-found']).pipe(
+                                        mergeMap((navigated: boolean): Observable<never> => {
+                                            return EMPTY;
                                         })
                                     );
+                                } else if (err.status >= 500 && err.status < 600) {
+                                    return this.dialog
+                                        .getRetryDialog({
+                                            msg: 'Error',
+                                        })
+                                        .pipe(
+                                            mergeMap((it: boolean): Observable<true> => {
+                                                return it ? of(it) : throwError((): ErrorTypes => err);
+                                            })
+                                        );
+                                }
                             }
-                        }
-                        return throwError((): ErrorTypes => err);
-                    })
-                );
+                            return throwError((): ErrorTypes => err);
+                        })
+                    );
+                },
             })
-        );
+        ) as MaybeAsync<T>;
     }
 
     public navigate(...args: Parameters<Router['navigate']>): Observable<boolean> {
