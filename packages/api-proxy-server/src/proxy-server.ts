a3b4a8ebb135703ab58c167cfaccfcb0fb2c0d16
donmahallem <donmahallem@users.noreply.github.com>
2025-03-08 22:36:15 +0100

    style(api-proxy-router): fix several styling issues

--- packages/api-proxy-server/src/proxy-server.ts
+++ packages/api-proxy-server/src/proxy-server.ts
@@ -57,7 +57,7 @@ export class ManniWatchProxyServer {
                 },
             })
         );
-        this.app.get('/api/live', (req, res, next) => {
+        this.app.get('/api/live', (req, res) => {
             res.json({ success: true });
         });
         this.app.use('/api', createApiProxyRouter(endpoint));
